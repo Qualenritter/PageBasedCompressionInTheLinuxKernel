8711d9a286985cb0beb307f512fefe4d1cf85812
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2017-07-17 09:33:39 +0200

    typo

--- test/run_benchmarks_final.sh
+++ test/run_benchmarks_final.sh
@@ -12,17 +12,17 @@ if [ "$?" -eq "0" ]; then
 	for sourcefilelength in 131072 35359176 69906992 104857600 211957760 302919680 468318452
 	do
 		echo "sourcefilelength=${sourcefilelength}"
-		insmod "b_compress_test_memcpy.ko" input_file_length=$sourcefilelength input_file_name="/dev/zero" input_file_name_short="zero" acceleration=1
+		insmod "compress_test_memcpy.ko" input_file_length=$sourcefilelength input_file_name="/dev/zero" input_file_name_short="zero" acceleration=1
 		if [ "$?" -ne "0" ]; then
-			rmmod "b_compress_test_memcpy"
+			rmmod "compress_test_memcpy"
 			make clean
 			exit -1
 		fi
-		rmmod "b_compress_test_memcpy"
+		rmmod "compress_test_memcpy"
 		for timemode in 1 2
 		do
-			insmod "b_compress_test_time_memcpy.ko" time_mode=$timemode compress_mode=1 input_file_length=$sourcefilelength output_file_length=$sourcefilelength input_file_name="/dev/zero" time_multiplicator=$targettime acceleration=1
-			rmmod "b_compress_test_time_memcpy"
+			insmod "compress_test_time_memcpy.ko" time_mode=$timemode compress_mode=1 input_file_length=$sourcefilelength output_file_length=$sourcefilelength input_file_name="/dev/zero" time_multiplicator=$targettime acceleration=1
+			rmmod "compress_test_time_memcpy"
 		done
 	done
 fi
